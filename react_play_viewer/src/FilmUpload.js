0ad56013d4e5ba35868d2927698076e81f03d657
Gabriel Clark <gabrielclark3330@gmail.com>
2024-01-23 01:00:16 -0600

    Finished mockup of fileupload

--- react_play_viewer/src/FilmUpload.js
+++ react_play_viewer/src/FilmUpload.js
@@ -1,14 +1,18 @@
 import { ArrowUpOnSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { Popover, Transition, RadioGroup } from "@headlessui/react";
 import { Fragment, useState, useCallback, useEffect, useRef } from "react";
-import { EmphasizedButton, EmphasizedButtonWithArrow, EmphasizedButtonWithBackArrow } from "./Buttons";
+import {
+  EmphasizedButton,
+  EmphasizedButtonWithArrow,
+  EmphasizedButtonWithBackArrow,
+} from "./Buttons";
 import { useDropzone } from "react-dropzone";
-import fbp1 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183131.png"
-import fbp2 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183250.png"
-import fbp3 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183302.png"
-import fbp4 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183033.png"
-import fbp5 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183046.png"
-import fbp6 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183120.png"
+import fbp1 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183131.png";
+import fbp2 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183250.png";
+import fbp3 from "./TeamOnePlayerPictures/Screenshot 2024-01-22 183302.png";
+import fbp4 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183033.png";
+import fbp5 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183046.png";
+import fbp6 from "./TeamTwoPlayerPictures/Screenshot 2024-01-22 183120.png";
 
 function formatTime(seconds) {
   const hours = Math.floor(seconds / 3600);
@@ -34,6 +38,28 @@ function formatTime(seconds) {
 export default function FilmUpload() {
   const [files, setFiles] = useState([]);
 
+  const [teamNameText, setTeamNameText] = useState("");
+  const inputRef = useRef(null);
+
+  const handleTeamNameChange = (event) => {
+    setTeamNameText(event.target.value);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [teamNameText]);
+
+  const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
+
+  let teamOnePlayerPictures = [fbp1, fbp2, fbp3];
+  let teamTwoPlayerPictures = [fbp4, fbp5, fbp6];
+
+  const [selectedTeam, setSelectedTeam] = useState(1);
+
+  const [games, setGames] = useState([]);
+
   async function handleFileDrop(droppedFiles) {
     const filePromises = droppedFiles.map((file) => {
       return new Promise((resolve, reject) => {
@@ -44,7 +70,6 @@ export default function FilmUpload() {
           video.onloadedmetadata = () => {
             window.URL.revokeObjectURL(video.src);
             const duration = video.duration;
-            console.log(duration);
             resolve({ name: file.name, duration: duration, file: file });
           };
 
@@ -68,22 +93,6 @@ export default function FilmUpload() {
     }
   }
 
-  const [teamNameText, setTeamNameText] = useState('');
-  const inputRef = useRef(null);
-
-  const handleTeamNameChange = (event) => {
-    console.log(event.target.value);
-    setTeamNameText(event.target.value);
-  };
-
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [teamNameText]);
-
-  const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
-
   const handleNextClick = () => {
     if (currentTaskIndex < taskSet.length - 1) {
       setCurrentTaskIndex(currentTaskIndex + 1);
@@ -96,9 +105,21 @@ export default function FilmUpload() {
     }
   };
 
+  const handleAddAnotherGame = () => {
+    setGames([...games, { files: [...files], selectedTeam: selectedTeam }]);
+    setFiles([]);
+    setCurrentTaskIndex(0);
+  };
+
+  const handleSubmitJob = () => {
+    if (files.length > 0) {
+      setGames([...games, { files: [...files], selectedTeam: selectedTeam }]);
+      setFiles([]);
+    }
+    console.log(games);
+  };
 
   function FilmUploadTask() {
-
     const { getRootProps, getInputProps } = useDropzone({
       onDrop: handleFileDrop,
     });
@@ -113,14 +134,12 @@ export default function FilmUpload() {
             <ArrowUpOnSquareIcon class="w-8 h-8 my-4 text-gray-500" />
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
               <p class="mb-2 text-sm text-gray-500">
-                <span class="font-semibold">Click to upload</span> or
-                drag and drop
+                <span class="font-semibold">Click to upload</span> or drag and
+                drop
               </p>
               <p class="text-xs text-gray-500">MP4</p>
             </div>
-            <input
-              {...getInputProps()}
-            />
+            <input {...getInputProps()} />
           </div>
         </div>
         <div className="relative grid gap-8 bg-white p-7 h-64 w-full overflow-auto">
@@ -130,9 +149,7 @@ export default function FilmUpload() {
               key={item.name}
             >
               <div className="ml-4">
-                <p className="text-sm font-medium text-gray-900">
-                  {item.name}
-                </p>
+                <p className="text-sm font-medium text-gray-900">{item.name}</p>
                 <p className="text-sm text-gray-600">
                   {formatTime(item.duration)}
                 </p>
@@ -146,7 +163,10 @@ export default function FilmUpload() {
           ))}
         </div>
         <div class="flex items-center justify-center w-full p-2 px-4 bg-white">
-          <input type="text" id="team_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:outline-none"
+          <input
+            type="text"
+            id="team_name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:outline-none"
             placeholder="Team Name"
             ref={inputRef}
             required
@@ -155,35 +175,49 @@ export default function FilmUpload() {
           />
         </div>
         <div className="flex flex-row bg-gray-50 p-4">
-          <div
-            className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none"
-          >
+          <div className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none">
             <span className="flex items-center">
               <span className="text-sm font-medium text-gray-900">
                 Film Upload
               </span>
             </span>
             <span className="block text-sm text-gray-600">
-              Upload every clip from the game or the entire game as 1 video and enter the name of the team to scout
+              Upload every clip from your game or the entire game as one clip
+              and enter the name of the team to scout
             </span>
           </div>
           <div className="ml-auto grid items-center flex-grow-0">
-            <EmphasizedButtonWithArrow text={"Next"} link={"#"} onClick={handleNextClick} />
+            <EmphasizedButtonWithArrow
+              text={"Next"}
+              link={"#"}
+              onClick={handleNextClick}
+            />
           </div>
         </div>
       </>
     );
   }
 
-  let teamOnePlayerPictures = [fbp1, fbp2, fbp3];
-  let teamTwoPlayerPictures = [fbp4, fbp5, fbp6];
-  const [selectedTeam, setSelectedTeam] = useState(1);
   const ImageColumn = ({ images, checked }) => {
     return (
-      <div className="flex flex-col justify-center space-y-4 bg-gray-50 border border-gray-300 rounded-lg block w-32 p-2.5 focus:outline-none">
+      <div
+        className={
+          "flex flex-col justify-center space-y-4 hover:bg-gray-100 border rounded-lg w-32 p-2.5 py-8 focus:outline-none" +
+          (checked
+            ? " bg-gray-200 hover:bg-gray-200 border-indigo-600"
+            : " bg-gray-50  border-gray-300")
+        }
+      >
         {images.map((image, index) => (
-          <div key={index} className="w-16 h-16 rounded-lg overflow-hidden self-center">
-            <img src={image} alt={`Image ${index}`} className="object-cover w-full h-full" />
+          <div
+            key={index}
+            className="w-16 h-16 rounded-lg overflow-hidden self-center"
+          >
+            <img
+              src={image}
+              alt={`Image ${index}`}
+              className="object-cover w-full h-full"
+            />
           </div>
         ))}
       </div>
@@ -194,32 +228,32 @@ export default function FilmUpload() {
     return (
       <>
         <div class="flex items-center justify-center w-full p-4 bg-white">
-          <div
-            className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none"
-          >
+          <div className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none">
             <span className="text-sm font-medium text-gray-900">
               {teamNameText}
             </span>
           </div>
         </div>
-        <div className="relative grid gap-8 bg-white p-7 h-80 w-full">
-            <RadioGroup value={selectedTeam} onChange={setSelectedTeam} className="flex justify-center space-x-8">
-      <RadioGroup.Option value={1}>
-        {({ checked }) => (
-          <ImageColumn images={teamOnePlayerPictures} checked={checked} />
-        )}
-      </RadioGroup.Option>
-      <RadioGroup.Option value={2}>
-        {({ checked }) => (
-          <ImageColumn images={teamTwoPlayerPictures} checked={checked} />
-        )}
-      </RadioGroup.Option>
-    </RadioGroup>
+        <div className="relative grid gap-8 bg-white px-7 py-4 h-80 w-full">
+          <RadioGroup
+            value={selectedTeam}
+            onChange={setSelectedTeam}
+            className="flex justify-center space-x-8"
+          >
+            <RadioGroup.Option value={1}>
+              {({ checked }) => (
+                <ImageColumn images={teamOnePlayerPictures} checked={checked} />
+              )}
+            </RadioGroup.Option>
+            <RadioGroup.Option value={2}>
+              {({ checked }) => (
+                <ImageColumn images={teamTwoPlayerPictures} checked={checked} />
+              )}
+            </RadioGroup.Option>
+          </RadioGroup>
         </div>
         <div className="flex flex-row bg-gray-50 p-4">
-          <div
-            className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none"
-          >
+          <div className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none">
             <span className="flex items-center">
               <span className="text-sm font-medium text-gray-900">
                 Team Description
@@ -230,17 +264,108 @@ export default function FilmUpload() {
             </span>
           </div>
           <div className="ml-auto grid items-center flex-grow-0 px-2">
-            <EmphasizedButtonWithBackArrow text={"Back"} link={"#"} onClick={handleBackClick} />
+            <EmphasizedButtonWithBackArrow
+              text={"Back"}
+              link={"#"}
+              onClick={handleBackClick}
+            />
           </div>
           <div className=" grid items-center flex-grow-0 px-2">
-            <EmphasizedButtonWithArrow text={"Next"} link={"#"} onClick={handleNextClick} />
+            <EmphasizedButtonWithArrow
+              text={"Next"}
+              link={"#"}
+              onClick={handleNextClick}
+            />
           </div>
         </div>
       </>
     );
   }
 
-  let taskSet = [<FilmUploadTask />, <TeamDescriptionTask />]
+  function ConfirmOrContinueTask() {
+    return (
+      <>
+        <div class="flex items-center justify-center w-full p-4 bg-white">
+          <div className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out focus:outline-none">
+            <span className="text-sm font-medium text-gray-900">
+              Continue Adding Games Or Submit
+            </span>
+          </div>
+        </div>
+        <div className="relative grid gap-8 bg-white p-7 h-64 w-full overflow-y-auto">
+          {games.map((game, index) => (
+            <div
+              className="-m-3 flex items-center p-2 transition duration-150 ease-in-out focus:outline-none border-b border-gray-200"
+              key={index}
+            >
+              <div className="ml-4">
+                <p className="text-sm font-medium text-gray-900 w-48 h-6 overflow-auto text-nowrap">
+                  {game.files.reduce((accumulator, file) => {
+                    return accumulator + file.name + " ";
+                  }, "")}
+                </p>
+                <p className="text-sm text-gray-600">
+                  {formatTime(
+                    game.files.reduce((accumulator, file) => {
+                      return accumulator + file.duration;
+                    }, 0)
+                  )}
+                </p>
+              </div>
+            </div>
+          ))}
+          <div
+            className="-m-3 flex items-center p-2 transition duration-150 ease-in-out focus:outline-none border-b border-gray-200"
+            key={-1}
+          >
+            <div className="ml-4">
+              <p className="text-sm font-medium text-gray-900 w-48 h-6 overflow-auto text-nowrap">
+                {files.reduce((accumulator, file) => {
+                  return accumulator + file.name + " ";
+                }, "")}
+              </p>
+              <p className="text-sm text-gray-600">
+                {formatTime(
+                  files.reduce((accumulator, file) => {
+                    return accumulator + file.duration;
+                  }, 0)
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row bg-gray-50 p-4">
+          <div className="mx-auto grid items-center flex-grow-0 px-2">
+            <EmphasizedButtonWithBackArrow
+              text={"Back"}
+              link={"#"}
+              onClick={handleBackClick}
+            />
+          </div>
+          <div className="mx-auto grid items-center flex-grow-0 px-2">
+            <EmphasizedButtonWithArrow
+              text={"Add Game"}
+              link={"#"}
+              onClick={handleAddAnotherGame}
+            />
+          </div>
+          <div className="mx-auto grid items-center flex-grow-0 px-2">
+            <EmphasizedButton
+              text={"Submit"}
+              link={"#"}
+              onClick={handleSubmitJob}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  let taskSet = [
+    <FilmUploadTask />,
+    <TeamDescriptionTask />,
+    <ConfirmOrContinueTask />,
+  ];
 
   return (
     <Popover>
--- react_play_viewer/src/HomePage.js
+++ react_play_viewer/src/HomePage.js
@@ -1,15 +1,43 @@
 import FilmUpload from "./FilmUpload";
 import HomePageHeader from "./HomePageHeader";
 import Footer from "./Footer";
+import ScoutingReportCard from "./SoutingReportOverviewCard";
 
 export default function HomePage() {
+  let imgs = [
+    "https://static.www.nfl.com/image/private/t_editorial_landscape_mobile/f_auto/league/xutbmai4wtzcndofvjc1.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThh1yca6tfHUSZJrLgtgIWI2ARlUFo-sZLjrmNIEd6r6jghHVZr9Ya1wBhZTJvfbMHYF0&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtR0D8LY2iTINW-dDKlvD9VVvx81t9t-L-UImm4TD8uWFfptZ9TFj1MW0vS9O7dtjojBE&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYTqxHkYBLZIDvWEliE_iBxGiwAG5Xb0wY4NIUCiSFGysHlgQJ8yEXfQIL1zHTbgfjq9g&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnsz6lpYzBSVHXIa69rOoF2ZzbZ57NM2eSOJ4K9PJ6e8wfSViYC-_LAZkZzoIj6lIUCas&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThf6BbK1KcDu5gKBVLnYioNrN4-jBBc63qqYM1mQvdGpBdCNv6jwMH9RBqRlTbIuw2_y4&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGo5PNn3Tg5CQzcsIFncch4zRCg6Kx_1WLhqNhKybrtaPh-0ZxrZQTBNDA41RDw-GK67Y&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBQzwsjHMEn8pmDgvEBw43SbZIcvRPZHtwiprFuCveYTUoAz8zGUfhobVn5f6hUf9da8s&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPMZP7U0go5IVE4W6n2uOecWy_z7-SbH2solgXyiBRrS-TyHkEcrLYAIv_rdUQLpVFVJk&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtR0D8LY2iTINW-dDKlvD9VVvx81t9t-L-UImm4TD8uWFfptZ9TFj1MW0vS9O7dtjojBE&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH5BhXZ_391SFBSMaMwpq-Z_u2gE8QR0fjeTBW257-C6k9l4nnT_uqXXQYVhxO2IONbkE&usqp=CAU"
+  ];
   return (
     <div className="bg-[#f5f5f7] min-h-screen flex flex-col">
-        <HomePageHeader/>
-        <div className="flex justify-center w-full my-14">
-            <FilmUpload/>
-        </div>
-        <Footer/>
+      <HomePageHeader />
+      <div className="flex justify-center w-full my-14">
+        <FilmUpload />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {imgs.map((img, index) => {
+            return(
+
+          <ScoutingReportCard
+            key={index}
+            link={"#"}
+            title={"Pius X"}
+            subtext={"2022-09-02"}
+            img={img}
+          />
+            );
+        })}
+      </div>
+      <Footer />
     </div>
   );
 }

d1c5c981fdab68b8b041f40aed04b6ad76f6be43
Gabriel Clark <gabrielclark3330@gmail.com>
2024-01-25 16:02:04 -0600

    Play diagram arrows

--- react_play_viewer/src/FieldRendering.js
+++ react_play_viewer/src/FieldRendering.js
@@ -1,11 +1,11 @@
 import { BundleCurve } from 'react-svg-curve';
 import footballField from './footballFieldDiagram.png';
-//import playerPaths from './SteelersExample.mp4_player_detections_json_string.json';
-//import playerClasses from './SteelersExample.mp4_player_classes_json_string.json';
-//import playerPaths from './CardsExample.mp4_player_detections_json_string.json';
-//import playerClasses from './CardsExample.mp4_player_classes_json_string.json';
-import playerPaths from './BillsExample.mp4_player_detections_json_string.json';
-import playerClasses from './BillsExample.mp4_player_classes_json_string.json';
+import playerPathsSteelers from './SteelersExample.mp4_player_detections_json_string.json';
+import playerClassesSteelers from './SteelersExample.mp4_player_classes_json_string.json';
+import playerPathsCards from './CardsExample.mp4_player_detections_json_string.json';
+import playerClassesCards from './CardsExample.mp4_player_classes_json_string.json';
+import playerPathsBills from './BillsExample.mp4_player_detections_json_string.json';
+import playerClassesBills from './BillsExample.mp4_player_classes_json_string.json';
 
 /*
 diagram_info = {
@@ -37,7 +37,7 @@ diagram_info = {
 
 const CircleWithLetter = ({ x, y, letter, color }) => {
   return (
-    <svg style={{ position: 'absolute', left: x-10, top: y-10 }}>
+    <svg style={{ position: 'absolute', left: x - 10, top: y - 10 }}>
       <circle cx={20} cy={20} r="15" fill={color} />
       <text x={20} y={20} fill="white" fontSize="20" textAnchor="middle" dy=".3em">{letter}</text>
     </svg>
@@ -48,22 +48,45 @@ const PathWithEnding = ({ data, color, beta }) => {
 
   return (
     <svg style={{ position: 'absolute', left: 0, top: 0 }} width="590" height="1215" stroke-width="5" strokeLinecap='round'>
+      <defs>
+        <marker
+          id="triangleB"
+          viewBox="0 0 10 10"
+          refX="1"
+          refY="5"
+          markerUnits="strokeWidth"
+          markerWidth="5"
+          markerHeight="5"
+          orient="auto">
+          <path d="M 0 0 L 10 5 L 0 10 z" fill="#E50101" />
+        </marker>
+        <marker
+          id="triangleR"
+          viewBox="0 0 10 10"
+          refX="1"
+          refY="5"
+          markerUnits="strokeWidth"
+          markerWidth="5"
+          markerHeight="5"
+          orient="auto">
+          <path d="M 0 0 L 10 5 L 0 10 z" fill="#00746B" />
+        </marker>
+      </defs>
       <BundleCurve
         data={data}
         beta={beta}
         showPoints={false}
         stroke={color}
+        markerEnd={color === "#00746B" ? "url(#triangleR)" : "url(#triangleB)"}
       />
     </svg>
   );
 }
 
-const MyImageComponent = () => {
+const MyImageComponent = ({playerClasses, playerPaths}) => {
   const offsetx = 90;
   const offsety = 0;
   const chartWidth = 590;
-  console.log(playerPaths);
-  console.log(playerClasses);
   const circles = [];
   const paths = [];
   //#395E66
@@ -75,18 +98,18 @@ const MyImageComponent = () => {
     var playerClass = playerClasses[trackid];
     const data = [];
     playerPaths[trackid].forEach(track => {
-      data.push([((chartWidth-track[0][0])+offsetx)*.75, track[0][1]+offsety]);
+      data.push([((chartWidth - track[0][0]) + offsetx) * .75, track[0][1] + offsety]);
     });
     if (playerClass === 2 || playerClass === 4) {
       circles.push(
-          <CircleWithLetter x={data[0][0]} y={data[0][1]} letter={playerLetters[playerClass]} color={classColors[playerClass]} />
-        );
+        <CircleWithLetter x={data[0][0]} y={data[0][1]} letter={playerLetters[playerClass]} color={classColors[playerClass]} />
+      );
       paths.push(
         <PathWithEnding data={data} beta={1} color={classColors[playerClass]} />
       );
     } else {
       circles.push(
-          <CircleWithLetter x={data[0][0]} y={data[0][1]} letter={playerLetters[playerClass]} color={classColors[playerClass]} />
+        <CircleWithLetter x={data[0][0]} y={data[0][1]} letter={playerLetters[playerClass]} color={classColors[playerClass]} />
       );
     }
   }
@@ -103,7 +126,9 @@ function Field() {
   return (
     <div className="App">
       <header className="App-header">
-        <MyImageComponent />
+        <MyImageComponent playerClasses={playerClassesBills} playerPaths={playerPathsBills}/>
+        <MyImageComponent playerClasses={playerClassesCards} playerPaths={playerPathsCards}/>
+        <MyImageComponent playerClasses={playerClassesSteelers} playerPaths={playerPathsSteelers}/>
       </header>
     </div>
   );
